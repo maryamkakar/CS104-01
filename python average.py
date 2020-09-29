15ef6bb3a7ecebf51d8a531ed679af93c8ce3e3f
maryamkakar <71049424+maryamkakar@users.noreply.github.com>
2020-09-29 14:46:38 -0400

    second commit

--- python average.py
+++ python average.py
@@ -1,5 +1,12 @@
-numberOfTests = int(input("PLease enter the number of tests you want to average: "))
-score1 = float(input("Please enter a score: "))
-score2 = floar(input("Please enter a score: "))
-average = (score1 + score2)/2
+numberOfTests = 0
+score = 0
+total = 0
+average = 0.0
+scoreCount = 0
+
+numberOfTests = int(input("Please enter the number of tests you want to average: "))
+score = int(input("Please enter a score: "))
+scoreCount = scoreCount + 1
+total = total + score
+average = (total/scoreScount
 print("The average is ",average)
