ceb84416c63a252929c3b34db6cb49ee69355a6b
maryamkakar <71049424+maryamkakar@users.noreply.github.com>
2020-09-29 14:36:24 -0400

    first commit

---
+++ python average.py
@@ -0,0 +1,13 @@
+
+numberOfTests = 0
+score = 0
+total = 0
+average = 0.0
+scoreCount = 0
+
+numberOfTests = int(input("Please enter the number of tests you want to average: "))
+score = int(input("Please enter a score: "))
+scoreCount = scoreCount + 1
+total = total + score
+average = (total/scoreScount
+print("The average is ",average)
