c37617e9a15b5d9df692af02f906bd42ed1fe298
maryamkakar <71049424+maryamkakar@users.noreply.github.com>
2020-09-29 14:45:18 -0400

    Add files via upload

---
+++ python average.py
@@ -0,0 +1,5 @@
+numberOfTests = int(input("PLease enter the number of tests you want to average: "))
+score1 = float(input("Please enter a score: "))
+score2 = floar(input("Please enter a score: "))
+average = (score1 + score2)/2
+print("The average is ",average)
